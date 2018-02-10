4a953d4272881e6af82274ee56882af74776b86a
susanxia1006 <susanxia1006@gmail.com>
2018-02-09 20:15:25 -0500

    Start to build skeletons for components and reducers.

---
+++ redux-keys/src/components/index.js
@@ -0,0 +1,3 @@
+import AppHeader from './AppHeader.js';
+
+export { AppHeader };
---
+++ redux-keys/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import KeyReducer from './KeyReducer.js';
+
+export default combineReducers({
+  KeyReducer
+});
